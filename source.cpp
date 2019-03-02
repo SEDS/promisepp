60aa412bfaf5527b6aaadc081690571c26dc5750
Nyalia Lui <luinyalia@gmail.com>
2019-03-02 13:36:54 -0500

    need to commit files

--- source.cpp
+++ source.cpp
@@ -13,24 +13,10 @@ int main(void)
 			settle.reject(std::logic_error("I did not get a car :("));
 	});
 
-	my_prom->then<std::string>([&](std::string value) {
+	my_prom->then([&](std::string value) {
 		std::cout << value << std::endl;
 		std::cout << "But does it have cup holders? ";
-		return Promises::Resolve('Y');
 	});
-
-	//starts new chain
-	auto another_prom = my_prom->then<std::string>([&](std::string value) {
-		std::cout << "The value is still " << value << std::endl;
-		return Promises::Resolve<std::string>("yes new chain");
-	});
-
-	try {
-		auto value = Promises::await<std::string>(another_prom);
-		std::cout << *value << std::endl;
-	} catch(std::exception &ex) {
-		std::cout << "main failure:" << ex.what() << std::endl;
-	}
 
 	return 0;
 }
